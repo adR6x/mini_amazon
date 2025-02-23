3fb7d3490928397e6531f7bb3b6e04dc58b2024a
Da Lin <dl402@duke.edu>
2025-02-23 16:03:40 -0500

    Added template

--- app/wishlist.py
+++ app/wishlist.py
@@ -7,6 +7,11 @@ from .models.product import Product
 from .models.purchase import Purchase
 from flask import redirect, url_for
 from .models.wishlist import WishlistItem
+from humanize import naturaltime
+
+def humanize_time(dt):
+    return naturaltime(datetime.datetime.now() - dt)
+
 
 from flask import Blueprint
 bp = Blueprint('wishlist', __name__)
@@ -18,7 +23,10 @@ def wishlist():
     items = WishlistItem.get_all_by_uid_since(current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
 
     if current_user.is_authenticated:
-        return jsonify([item.__dict__ for item in items])
+        return render_template('wishlist.html',
+                       items=items,
+                       humanize_time=humanize_time)
+
     else:
        return jsonfiy({}), 404
 
