5e79a47028855b690c2e7ca3431e2b5d930dd58c
adR6 <ad641@duke.edu>
2025-03-10 00:37:30 -0400

    workable product_all html and css

---
+++ app/product.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from flask_login import current_user
+import datetime
+
+from .models.product import Product
+from .models.purchase import Purchase
+
+from flask import Blueprint
+bp = Blueprint('product', __name__)
+
+
+@bp.route('/product_all', methods=['GET', 'POST'])
+def product_all():
+    return render_template('product_all.html')
