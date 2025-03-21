cd02a1ac99a3330e7137f8e1c7ec78c0aa4e8818
Da Lin <dl402@duke.edu>
2025-03-21 17:44:06 -0400

    Added navigation bar to base template

--- app/__init__.py
+++ app/__init__.py
@@ -20,10 +20,7 @@ def create_app():
     
     from .wishlist import bp as wishlist_bp
     app.register_blueprint(wishlist_bp)
-
-    from .products import bp as products_bp
-    app.register_blueprint(products_bp)
-
+    
     from .users import bp as user_bp
     app.register_blueprint(user_bp)
 
