3147303a98fafbea39d9d819e4a7f7338556ac80
Da Lin <dl402@duke.edu>
2025-03-23 21:15:43 -0400

    Deleted Purchase and login UI is changed

--- app/index.py
+++ app/index.py
@@ -13,14 +13,6 @@ bp = Blueprint('index', __name__)
 def index():
     # get all available products for sale:
     products = Product.get_all()
-    # find the products current user has bought:
-    if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_uid_since(
-            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    else:
-        purchases = None
     # render the page by adding information to the index.html file
-    print(products)
     return render_template('index.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+                           avail_products=products)
