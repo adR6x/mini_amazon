c785cfc05110487ed77d987f55f83b10d3b64523
Tianze Ren <tr158@duke.edu>
2025-03-24 22:41:55 -0400

    inventory front end

--- app/inventory_routes.py
+++ app/inventory_routes.py
@@ -1,21 +1,16 @@
 from flask import Blueprint, jsonify
 from app.models.inventory import Inventory
+from flask_login import current_user
+from flask import render_template, request
+
 
 inventory_bp = Blueprint('inventory', __name__)
 
-@inventory_bp.route('/seller/<int:seller_id>/inventory', methods=['GET'])
-def get_seller_inventory(seller_id):
-    inventory = Inventory.get_by_seller(seller_id)
-    if not inventory:
-        return jsonify({"message": "No products found for this seller"}), 404
+@inventory_bp.route('/inventory_page')
 
-    return jsonify([
-        {
-            "seller_id": item.seller_id,
-            "product_id": item.product_id,
-            "product_name": item.product_name,
-            "quantity_available": item.quantity_available,
-            "price": float(item.price)
-        }
-        for item in inventory
-    ]), 200
+def inventory_page():
+    seller_id = current_user.id
+    inventory = Inventory.get_by_seller(seller_id)
+    # print(seller_id)
+    # print(inventory)
+    return render_template("inventory.html", inventory=inventory)
--- app/models/inventory.py
+++ app/models/inventory.py
@@ -10,11 +10,13 @@ class Inventory:
 
     @staticmethod
     def get_by_seller(seller_id):
+        print(seller_id)
         rows = app.db.execute("""
-            SELECT i.seller_id, i.product_id, p.short_name, i.quantity_available, i.price
+            SELECT i.seller_id, i.product_id, p.name, i.quantity_available, i.price
             FROM Inventory i
             JOIN Products p ON i.product_id = p.product_id
             WHERE i.seller_id = :seller_id
         """, seller_id=seller_id)
+        print(rows)
 
         return [Inventory(*row) for row in rows] if rows else []
